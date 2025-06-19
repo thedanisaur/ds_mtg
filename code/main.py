0e069faf4f902384892872f995b7a6cdc32f4a25
Daniel DeFinis <definisdan@yahoo.com>
2025-06-19 17:58:02 -0500

    Convert the malformed json to yaml

--- code/main.py
+++
@@ -1,7 +0,0 @@
-def fix_json_keys():
-    with open('cards/artifact/artifact_cards.json', 'r') as file:
-        content = file.read()
-        print(content)
-
-if __name__ == '__main__':
-    fix_json_keys()
