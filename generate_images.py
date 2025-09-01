10fd16d041a6c021e569aa2586fa11982bb4defc
Daniel DeFinis <definisdan@yahoo.com>
2025-09-01 12:16:23 -0500

    Update card balance and add script to generate images in markdown.
    Functional Changes:
    Halberd: Reduce equip cost from 2 to 1
    Talisman of the Way of White: Reduce casting cost from WW to W
    Wolf Knight's Greatshield: Reduce casting cost from 3 to 2
    Chosen Undead: Update ability to force sacrifice of a planeswalker or legendary creature instead of just legendary creature
    Zombie Hollow: You now get a soul when it leaves the battlefield. When it returns it now gives -1/-1 to another creature
    Undead Hound: Now punishes a player for having souls
    Vordt and the Dancer: Now has indestructible during combat
    Hollow: Instead of a vanilla creature you now receive souls if it leaves the battlefield
    Baneful Queen, Mytha: Casting cost reduced by 1
    Carim Guardian: Casting cost reduced by 1
    Alluring Skull: Now draws a card if the creature dies
    Contrasted Curse: Now punishes the player for having souls
    Ent of the Darkroot Garden: Cost reduced and changed from 2G to GG
    Hollow Knight: Now gives souls when leaving the battlefield and has regrowth ability
    Shaded Woods: Is now an Instant
    Mutant Hollow: Additional cost to cast reduced from 3 to 2
    Flamewielding Hollow: Now gives you a soul when it leaves the battlefield
    Profuse Sweat: Add kicker that draws a card and prevents bleed, now you have to choose between moving a curse and less damage being dealt
    
    Replaced Cards:
    Creeping Corrosion -> Convenant of the Forest Hunter
    Sunlight Blade -> Solaire of Astora
    
    Renamed Cards:
    Ent -> Ent of the Darkroot Garden
    Flamewielding Undead -> Flamewielding Hollow

---
+++ generate_images.py
@@ -0,0 +1,41 @@
+if __name__ == '__main__':
+    import os
+
+    # Configurations
+    folder_path = "./cards"
+    markdown_file = "README.md"
+    section_header = "## Full Set"
+    order = [ 'colorless', 'white', 'blue', 'black', 'red', 'green', 'gold', 'artifact', 'land', 'token', ]
+
+    # Supported image types
+    image_extensions = ('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.svg', '.webp')
+
+    # Get sorted list of image files
+    count = 0
+    image_markdown = '\n'
+    for folder in order:
+        for file in sorted(os.listdir(f"{folder_path}/{folder}/")):
+            if file.lower().endswith(image_extensions):
+                formatted_title = file.rsplit('.', 1)[0].replace('_', ' ').title()
+                # Generate Markdown for images
+                # image_markdown += f"![{formatted_title}]({folder_path}/{folder}/{file})\n"
+                image_markdown += f'<img src="{folder_path}/{folder}/{file}" alt="{formatted_title}" width="400" />&nbsp;'
+
+    # Read the existing Markdown file
+    with open(markdown_file, "r", encoding="utf-8") as file:
+        content = file.read()
+
+    # Find the position of the header
+    header_index = content.find(section_header)
+    if header_index == -1:
+        raise ValueError(f"Header '{section_header}' not found in {markdown_file}")
+
+    # Keep content up to the header and that line
+    header_line_end = content.find("\n", header_index) + 1
+    new_content = content[:header_line_end] + "\n" + image_markdown + "\n"
+
+    # Write back the updated content
+    with open(markdown_file, "w", encoding="utf-8") as file:
+        file.write(new_content)
+
+    print(f"✅ Updated '{markdown_file}' with image list from '{folder_path}' after '{section_header}'")
